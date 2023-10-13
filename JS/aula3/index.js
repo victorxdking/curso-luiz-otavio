d0a0fac95cf97b811f44141dcf9773cbad12f594
victorxdking <victorxdking@gmail.com>
2023-10-13 11:44:12 -0300

    ATT

---
+++ JS/aula3/index.js
@@ -0,0 +1,7 @@
+console.log('Victor Vieira');
+console.log("Victor Vieira");
+console.log(`Victor Vieira`);
+
+console.log(35, 15.85, 'Luiz Otávio De Novo');
+
+console.log('Meu nome é "Victor". Estou aprendendo JavaScript às', 10, 'da manhã.');
