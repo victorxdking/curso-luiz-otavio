8fe4f3363cdb1cee242f03ef57d4219a2a649372
victorxdking <victorxdking@gmail.com>
2023-10-17 18:17:38 -0300

    aula 9 att

--- JS/aula9/index.js
+++ JS/aula9/index.js
@@ -1,3 +1,12 @@
 const nome = 'Victor'; // string
 const nome1 = "Victor"; // string
 const nome2 = `Victor`; // string
+const num1 = 10; // number
+const num2 = 10.52; // number
+let nomeAluno; // undefined -> não aponta para local nenhum da memória
+const sobrenomeAluno = null; // Nulo -> não aponta para local nenhum da memória
+const aprovado = false; // Bolean = true, false (lógico)
+
+const a = 2;
+const b = a;
+console.log(a, b); // 2,2
