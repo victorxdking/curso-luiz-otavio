e5246ca91d79571adf61a68c274ab20998a8f1a3
victorxdking <victorxdking@gmail.com>
2023-10-19 23:36:00 -0300

    aula11

--- JS/aula10/index.js
+++ JS/aula10/index.js
@@ -1,7 +1,42 @@
 /*
-* Aritméticos
-* + Adição / Concatenação 
-* ** Potenciação
-* % Resto da divisão
+() Parêntese
+** Potenciação
+* / % 
++ -
+Incremento ++
+Decremento --
 */
+
+/*
+const num1 = 5;
+const num2 = 2;
+const num3 = 10;
+console.log((num1 + num2) * num3)
+
+let contador = 1;
+console.log(++contador);
+console.log(contador++);
+console.log(contador--);
+
+let contador2 = 1;
+contador2++
+console.log(contador2);
+
+const passo = 2;
+let contador3 = 0;
+contador3 = contador3 + passo;
+console.log(contador3);
+
+
+let contador4 = 0;
+contador4 += 2;
+contador4 += 2;
+contador4 += 2;
+console.log(contador4);
+*/
+
+// NaN - Not a number, parseInt (inteiro), parseFloat(decimais), Number
 const num1 = 10;
+const num2 = Number('5');
+console.log(num1 + num2);
+console.log(typeof num2);
