1faf67bb6120c5b9b160855287f3c204dadacd68
victorxdking <victorxdking@gmail.com>
2023-10-26 16:27:10 -0300

    aula 17

---
+++ JS/aula16/index.js
@@ -0,0 +1,33 @@
+//                0        1        2         3        4
+const alunos = ['Luiz', 'Maria', 'João']; // Luiza, Eduardo
+
+console.log(typeof alunos);
+
+
+
+// alunos.push('Luiza');
+// alunos.push('Eduardo');
+
+// console.log(alunos.slice(0, -2));
+
+// console.log(alunos[50]);
+
+// delete alunos[1];
+// console.log(alunos[1]);
+
+// alunos.unshift('Luiza'); // Adiona no começo
+// alunos.unshift('Fábio');
+
+// alunos.push('Luiza'); // Adiciona no fim
+// alunos.push('Fábio'); 
+
+// alunos[alunos.length] = 'Luiza'; // Adiciona no fim
+// alunos[alunos.length] = 'Fábio';
+// alunos[alunos.length] = 'Luana';
+
+// alunos[0] = 'Eduardo'; // Altera
+// alunos[3] = 'Luiza'; // Adicionando
+
+// console.log(alunos);
+// console.log(alunos[0]);
+// console.log(alunos[2]);
---
+++ JS/aula17/index.js
@@ -0,0 +1,27 @@
+// function soma(x = 1, y = 1) {
+//     const resultado = x + y;
+//     return resultado;
+// }
+
+// const resultado =  soma (4, 2);
+// console.log(resultado);
+
+
+// // função anônima
+//  const raiz = function (n) {
+//     return n ** 0.5;
+//  };
+
+//  console.log(raiz(9));
+//  console.log(raiz(16));
+//  console.log(raiz(25));
+
+
+// Arrow Function
+ const raiz = (n) => {
+    return n ** 0.5;
+ }
+ 
+ console.log(raiz(9));
+ console.log(raiz(16));
+ console.log(raiz(25));
