9338df559a3319539f22c0a3a0c7b0042787bc2e
victorxdking <victorxdking@gmail.com>
2023-10-27 10:56:30 -0300

    aula 18

--- JS/aula18/index.js
+++ JS/aula18/index.js
@@ -1,7 +1,23 @@
 const pessoa1 = {
     nome: 'Victor',
     sobrenome: 'Vieira',
-    idade: '26'
+    idade: '26',
+
+    fala() {
+        console.log(`A minha idade atual é ${this.idade}.`);
+    },
+
+    incrementaIdade() {
+        this.idade++;
+    }
 };
 
-
+pessoa1.fala();
+pessoa1.incrementaIdade();
+pessoa1.fala();
+pessoa1.incrementaIdade();
+pessoa1.fala();
+pessoa1.incrementaIdade();
+pessoa1.fala();
+pessoa1.incrementaIdade();
+pessoa1.fala();
