d5b94b432bd89c357ad876ed5f9dfd34df2a7b4d
victorxdking <victorxdking@gmail.com>
2023-11-01 15:08:17 -0300

    Operadores de comparação

---
+++ JS/aula21/index.js
@@ -0,0 +1,13 @@
+/* 
+Operadores de comparação
+> maior que
+>= maio que ou igual a
+< menor que
+<= menor que ou igual a 
+== igualdade (valor) **********
+=== igualdade estrita (valor e tipo)
+!= diferente (valor) **********
+!== diferente estrito (valor e tipo)
+*/
+
+const comp = 
