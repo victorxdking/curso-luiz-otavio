b110e955d6f3a8aa05dabecea1de83a820f6d160
victorxdking <victorxdking@gmail.com>
2023-11-01 15:44:55 -0300

    Operadores Lógicos

---
+++ JS/aula22/inedx.js
@@ -0,0 +1,21 @@
+/*
+Operadores Lógicos
+&& -> AND -> Todas as expressões precisam ser verdadeiras para retornar true
+|| -> OR
+! -> NOT
+*/
+
+// const expressaoAnd = true &&  true && true && true;
+// console.log(expressaoAnd);
+
+// const expressaoOr = true || false || false || false;
+// console.log(expressaoOr);
+
+// const usuario = 'Victor'; // form usuário digitou
+// const senha = '12345'; // form usuário digitou
+
+// //                             true                  true
+// const vaiLogar = usuario === 'Victor' && senha === '12345';
+// console.log(vaiLogar);
+
+// console.log(!true);
