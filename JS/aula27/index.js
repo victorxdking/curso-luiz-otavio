29b68f700da25a557ce3cdf2883113f747604929
victorxdking <victorxdking@gmail.com>
2023-11-02 15:51:15 -0300

    Operação ternária

---
+++ JS/aula27/index.js
@@ -0,0 +1,14 @@
+// (condição) ? 'Valor para verdadeiro' : 'Valor para falso';
+const pontuacaoUsuario = 1000;
+const nivelUsuario = pontuacaoUsuario >= 1000 ? 'Usuário VIP' : 'Usuário normal';
+
+const corUsuario = 'Pink';
+const corPadrao = corUsuario || 'Preta';
+
+console.log(nivelUsuario, corPadrao);
+
+// if (pontuacaoUsuario >= 1000) {
+//     console.log('Usuário VIP');
+// } else {
+//     console.log('Usuário normal');
+// }
