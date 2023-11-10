a2538746905ce7861bd5610358e33d98b1e54d98
victorxdking <victorxdking@gmail.com>
2023-11-10 17:36:41 -0300

    Break e Continue

---
+++ JS/aula40/aula40.js
@@ -0,0 +1,19 @@
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// Continue continua para próxima iteração
+// Break sai do laço
+
+for (let numero of numeros) {
+    
+    if (numero === 2) {
+        console.log('Pulei o número 2')
+        continue;
+    }
+
+    console.log(numero);
+    
+    if (numero === 7) {
+        console.log('7 encontrado, saindo...')
+        break;
+    }
+}
