3bea67fdf3872e2651b281f35c91ee18a73887f0
victorxdking <victorxdking@gmail.com>
2023-11-14 19:22:47 -0300

    Exercício com lógica de programação - 03

--- JS/aula43/aula43.js
+++ JS/aula43/aula43.js
@@ -5,3 +5,17 @@
 // Número NÃO é divisível por 3 e 5 = Retorna o próprio número
 // Chegar se o número é realmente um número = Retorna o próprio número
 // Use a função com números de 0 a 100
+
+function fizzBuzz(numero) {
+    if (typeof numero !== 'number') return numero;
+    if (numero % 3 === 0 && numero % 5 === 0) return 'FizzBuzz'; 
+    if (numero % 3 === 0) return 'Fizz'; 
+    if (numero % 5 === 0) return 'Buzz'; 
+    return numero;
+}
+
+console.log('a', fizzBuzz('a'));
+
+for (let i = 0; i <= 100; i++) {
+    console.log(i, fizzBuzz(i))
+}
