c70609d973f04ec0edb84fa4dacb5a047b2d7b65
victorxdking <victorxdking@gmail.com>
2023-11-15 14:37:38 -0300

    Exercício - Criando um timer com setInterval

--- JS/aula47/assets/js/main.js
+++ JS/aula47/assets/js/main.js
@@ -1,26 +1,43 @@
-function criaHoraDosSegundos(segundos) {
-    const data = new Date(segundos);
-    return data.toLocaleTimeString('pr-BR', {
-        hour12: false
+function relogio() {
+    function criaHoraDosSegundos(segundos) {
+        const data = new Date(segundos * 1000);
+        return data.toLocaleTimeString('pr-BR', {
+            hour12: false,
+            timeZone: 'UTC'
+        });
+    }
+
+    const relogio = document.querySelector('.relogio');
+    let segundos = 0;
+    let timer;
+    
+    function iniciaRelogio() {
+        timer = setInterval(function() {
+            segundos++;
+            relogio.innerHTML = criaHoraDosSegundos(segundos);
+        }, 1000);
+    }
+    
+    document.addEventListener('click', function(e) {
+        const el = e.target;
+    
+        if(el.classList.contains('zerar')) {
+            clearInterval(timer);
+            relogio.innerHTML = '00:00:00';
+            relogio.classList.remove('pausado');
+            segundos = 0;
+        }
+        
+        if(el.classList.contains('iniciar')) {
+            relogio.classList.remove('pausado');
+            clearInterval(timer);
+            iniciaRelogio();
+        }
+        
+        if(el.classList.contains('pausar')) {
+            clearInterval(timer);
+            relogio.classList.add('pausado');
+         }
     });
 }
-
-console.log(criaHoraDosSegundos(10));
-
-const relogio = document.querySelector('.relogio');
-const iniciar = document.querySelector('.iniciar');
-const pausar = document.querySelector('.pausar');
-const zerar = document.querySelector('.zerar');
-
-iniciar.addEventListener('click', function (event) {
-    relogio.innerHTML = alert('cliquei no iniciar');
-});
-
-pausar.addEventListener('click', function (event) {
-    alert('cliquei no pausar');
-});
-
-zerar.addEventListener('click', function (event) {
-    alert('cliquei no zerar');
-});
-
+relogio();
