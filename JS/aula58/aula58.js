c6bb44d0b15ea3f649a317d09bb5d5558fab1ce5
victorxdking <victorxdking@gmail.com>
2023-11-19 21:34:40 -0300

    Funções construtoras (constructor functions)

---
+++ JS/aula58/aula58.js
@@ -0,0 +1,22 @@
+// Função construtora -> objetos
+// Função fábrica -> objetos
+// Construtora -> Pessoa (new)
+function Pessoa(nome, sobrenome) {
+    // Atributos ou métodos privados
+    const ID = 123456;
+    const metodoInterno = function() {
+
+    };
+
+    // Atributos ou métodos públicos
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+
+    this.metodo = function() {
+        console.log(this.nome + ': sou um método');
+    };
+}
+
+const p1 = new Pessoa('Victor', 'Vieira');
+const p2 = new Pessoa('Luiz', 'Otávio');
+p2.metodo();
