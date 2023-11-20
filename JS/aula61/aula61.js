4c2080ca38a6f3fca89ef55a4e36005b3abfd8f1
victorxdking <victorxdking@gmail.com>
2023-11-20 14:30:00 -0300

    Funções geradoras

---
+++ JS/aula61/aula61.js
@@ -0,0 +1,93 @@
+// Funções geradoras
+// 1º-------------------------------------------------------------------------------
+/* function* geradora1() {
+    // Código qualquer...
+    yield 'Valor 1';
+    // Código qualquer...
+    yield 'Valor 2';
+    // Código qualquer...
+    yield 'Valor 3';
+}
+
+function* geradora2() {
+    let i = 0;
+
+    while(true) {
+        yield i;
+        i++;      
+    }
+}
+const g2 = geradora2();
+console.log(g2.next().value);
+console.log(g2.next().value);
+console.log(g2.next().value);
+console.log(g2.next().value);
+console.log(g2.next().value);
+console.log(g2.next().value);
+console.log(g2.next().value);
+console.log(g2.next().value); 
+
+
+// 2º-------------------------------------------------------------------------------
+function* geradora3() {
+    yield 0;
+    yield 1;
+    yield 2;
+}
+
+function* geradora4() {
+    yield* geradora3();
+    yield 3;
+    yield 4;
+    yield 5;
+}
+
+const g4 = geradora4();
+for(let valor of g4) {
+    console.log(valor);
+}
+// ------------------------------------------------------------------------------- */
+
+
+// 3º-------------------------------------------------------------------------------
+function* geradora3() {
+    yield 0;
+    yield 1;
+    yield 2;
+}
+
+function* geradora4() {
+    yield* geradora3();
+    yield 3;
+    yield 4;
+    yield 5;
+}
+
+const g4 = geradora4();
+
+function* geradora5() {
+    yield function() {
+        console.log('Vim do y1');
+    };
+
+    yield function() {
+        console.log('Vim do return');
+    }
+
+    //...
+
+    yield function() {
+        console.log('Vim do y3');
+    };
+}
+
+const g5 = geradora5();
+const func1 = g5.next().value;
+const func2 = g5.next().value;
+const func3 = g5.next().value;
+
+func1();
+func2();
+func3();
+
+
