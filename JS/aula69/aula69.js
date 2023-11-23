e3045fd9e10de2c435dd43779085d1a1fe0f33fa
victorxdking <victorxdking@gmail.com>
2023-11-23 17:28:14 -0300

    forEach

---
+++ JS/aula69/aula69.js
@@ -0,0 +1,29 @@
+/* 
+const a1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+for(let valor of a1) {
+    console.log(valor);
+}
+ */
+
+
+
+/* 
+const a1 = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+a1.forEach(function(valor, indice, array){
+    console.log(valor, indice, array);
+});
+ */
+
+
+
+/* 
+const a1 = [10, 20, 30];
+let total = 0;
+
+a1.forEach(valor => {
+    total += valor;
+});
+
+console.log(total);
+ */
