612c554ff665dd55cca112455189f9c4516816c0
victorxdking <victorxdking@gmail.com>
2023-12-27 21:59:51 -0300

    Exercício - Validando um CPF (Solução)

--- JS/aula77/aula77.js
+++ JS/aula77/aula77.js
@@ -13,8 +13,61 @@ Se o digito for maior que 9, consideramos 0.
 
 11 - (284 % 11) = 2 (Segundo dígito)
 Se o digito for maior que 9, consideramos 0.
-*/
+
 let cpf = '705.484.450-52';
 let cpfLimpo = cpf.replace(/\D+/g, '');
 cpfArray = Array.from(cpfLimpo);
 console.log(cpfArray.reduce((ac, val) => ac + Number(val), 0));
+
+// Isso é uma expressão regular que representa qualquer coisa que não seja número -> /\D+/g
+// Dessa forma sempre que for encontrado um caracter no CPF que não seja número será substituído por '' nada.
+*/
+
+function ValidaCPF(cpfEnviado) {
+    Object.defineProperty(this, 'cpfLimpo', {
+        enumerable: true,
+        get: function() {
+            return cpfEnviado.replace(/\D+/g, '');
+        }
+    });
+}
+
+ValidaCPF.prototype.valida = function() {
+    if(typeof this.cpfLimpo === 'undefined') return false;
+    if(this.cpfLimpo.length !== 11) return false;
+    if(this.isSequencia()) return false;
+    
+    const cpfParcial = this.cpfLimpo.slice(0, -2);
+    const digito1 = this.criaDigito(cpfParcial);
+    const digito2 = this.criaDigito(cpfParcial + digito1);
+
+    const novoCpf = cpfParcial + digito1 + digito2
+    return novoCpf === this.cpfLimpo;
+};
+
+ValidaCPF.prototype.criaDigito = function(cpfParcial) {
+    const cpfArray = Array.from(cpfParcial);
+    
+    let regressivo = cpfArray.length + 1;
+    const total = cpfArray.reduce((ac, val) => {
+        ac += (regressivo * Number(val));
+        regressivo--;
+        return ac;
+    }, 0);
+
+    const digito = 11 - (total % 11);
+    return digito > 9 ? '0' : String(digito);
+};
+
+ValidaCPF.prototype.isSequencia = function() {
+    const sequencia = this.cpfLimpo[0].repeat(this.cpfLimpo.length);
+    return sequencia === this.cpfLimpo;
+};
+
+const cpf = new ValidaCPF('705.484.450-52');
+
+if(cpf.valida()) {
+    console.log('CPF válido');
+} else {
+    console.log('CPF inválido');
+}
