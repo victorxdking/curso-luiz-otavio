1f710ce5dcb333bbd2578ccafe525da4f7ada2ce
victorxdking <victorxdking@gmail.com>
2024-01-11 16:23:48 -0300

    Módulos (2)

---
+++ JS/node/aula2/app.js
@@ -0,0 +1,15 @@
+/* const Cachorro = require('./z/mod2');
+
+const c1 = new Cachorro('Dog');
+c1.latir();
+
+// ir para frente ./B/arquivo
+// voltar uma pasta ../B/arquivo */
+
+/* const path = require('path');
+ console.log(path.resolve(__dirname, '.', '.', 'arquivos', 'imagens')); */
+
+ const Cachorro = require('./z/mod2');
+
+ const c1 = new Cachorro('Dog');
+ c1.latir();
